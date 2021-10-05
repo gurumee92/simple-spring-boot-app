979c85543faec566bbeac09ccd325d9e296cbf56
1101320 <gurumee92@gmail.com>
2021-10-05 10:33:41 +0900

    Add IndexController

---
+++ src/test/java/com/gurumee/simplespringbootapp/controllers/IndexControllerTest.java
@@ -0,0 +1,16 @@
+package com.gurumee.simplespringbootapp.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest
+class IndexControllerTest {
+    @Test
+    @DisplayName("context load")
+    public void contextLoad() {
+
+    }
+}
