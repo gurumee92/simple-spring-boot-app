c8129e2c682dc88792fc90e06f61672430de5f3b
1101320 <gurumee92@gmail.com>
2021-10-05 11:16:14 +0900

    Add GenerateRandomService

--- src/main/java/com/gurumee/simplespringbootapp/controllers/IndexController.java
+++ src/main/java/com/gurumee/simplespringbootapp/controllers/IndexController.java
@@ -1,14 +1,38 @@
 package com.gurumee.simplespringbootapp.controllers;
 
+import com.gurumee.simplespringbootapp.apis.services.GenerateRandomNumberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.http.HttpResponse;
+
 @RestController
-@RequestMapping("/")
 public class IndexController {
-    @GetMapping()
-    public String index() {
-        return "";
+    private final GenerateRandomNumberService generateRandomNumberService;
+
+    public IndexController(GenerateRandomNumberService generateRandomNumberService) {
+        this.generateRandomNumberService = generateRandomNumberService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> index() {
+        int randomNumber = generateRandomNumberService.generate();
+
+        switch (randomNumber % 4) {
+            case 0:
+                return ResponseEntity.status(HttpStatus.OK).body("ok - 2xx");
+            case 1:
+                return ResponseEntity.status(HttpStatus.FOUND).body("redirect - 3xx");
+            case 2:
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("error - 4xx");
+            case 3:
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("error - 5xx");
+        }
+
+        throw new RuntimeException("logic error");
     }
 }
--- src/test/java/com/gurumee/simplespringbootapp/controllers/IndexControllerTest.java
+++ src/test/java/com/gurumee/simplespringbootapp/controllers/IndexControllerTest.java
@@ -1,16 +1,65 @@
 package com.gurumee.simplespringbootapp.controllers;
 
+import com.gurumee.simplespringbootapp.apis.services.GenerateRandomNumberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.hamcrest.Matchers.containsString;
 
-@WebMvcTest
+@WebMvcTest(IndexController.class)
 class IndexControllerTest {
-    @Test
-    @DisplayName("context load")
-    public void contextLoad() {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @MockBean
+    private GenerateRandomNumberService generateRandomNumberService;
+
+    @Test
+    @DisplayName("GET / 테스트 - 2xx")
+    public void get_index_page_2xx() throws Exception {
+        when(generateRandomNumberService.generate()).thenReturn(0);
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("ok - 2xx")));
+    }
+
+    @Test
+    @DisplayName("GET / 테스트 - 3xx")
+    public void get_index_page_3xx() throws Exception {
+        when(generateRandomNumberService.generate()).thenReturn(1);
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(content().string(containsString("redirect - 3xx")));
+    }
+
+    @Test
+    @DisplayName("GET / 테스트 - 4xx")
+    public void get_index_page_4xx() throws Exception {
+        when(generateRandomNumberService.generate()).thenReturn(2);
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().string(containsString("error - 4xx")));
+    }
+
+    @Test
+    @DisplayName("GET / 테스트 - 5xx")
+    public void get_index_page_5xx() throws Exception {
+        when(generateRandomNumberService.generate()).thenReturn(3);
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().is5xxServerError())
+                .andExpect(content().string(containsString("error - 5xx")));
     }
 }
