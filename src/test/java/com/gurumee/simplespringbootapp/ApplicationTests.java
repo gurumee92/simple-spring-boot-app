a527b88be22ec61250b30a6dd3332709083e633e
1101320 <gurumee92@gmail.com>
2021-10-05 10:24:44 +0900

    FIRST INIT

---
+++ src/test/java/com/gurumee/simplespringbootapp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurumee.simplespringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
